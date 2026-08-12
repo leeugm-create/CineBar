07870b811c04ed10bc1c53f0c7ffdabc89805d51
leeugm-create <leeugm@gmail.com>
2026-08-12 22:46:09 +0800

    release-prep: Build 88 (composer置底+右键菜单)

--- CineBarWebsite/app/home.tsx
+++ CineBarWebsite/app/home.tsx
@@ -4,7 +4,7 @@ import SiteSearch from "./site-search";
 import ThemeToggle from "./theme-toggle";
 
 const releaseURL =
-  "https://cinebar.cc/downloads/CineBar-0.9.0-test-build-87-universal.zip";
+  "https://cinebar.cc/downloads/CineBar-0.9.0-test-build-88-universal.zip";
 const releasesURL = "https://github.com/leeugm-create/CineBar/releases";
 
 function ratingIcon(id: string) {
