044280d6cfff5a38d2ccf75fc3bb79c8b1acb83a
leeugm-create <leeugm@gmail.com>
2026-08-13 20:58:43 +0800

    release-prep: Build 112 (CineAI透明修复)

--- CineBarWebsite/app/hero-trending.tsx
+++ CineBarWebsite/app/hero-trending.tsx
@@ -11,7 +11,7 @@ type TrendingMovie = {
 };
 
 const releaseURL =
-  "https://cinebar.cc/downloads/CineBar-0.9.0-test-build-111-universal.zip";
+  "https://cinebar.cc/downloads/CineBar-0.9.0-test-build-112-universal.zip";
 
 const isChinese = (locale: Locale) => locale === "zh-Hans" || locale === "zh-Hant";
 
